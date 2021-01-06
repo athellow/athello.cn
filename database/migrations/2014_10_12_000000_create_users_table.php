bf087e922c6a1be1e89c087e2765228da6747fb2
Athellow <athellow@outlook.com>
2021-01-06 18:49:35 +0800

    no message

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->default('');
-            $table->string('email')->unique()->default('');
+            $table->string('email')->default(''); //->unique()
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->default('');
             $table->rememberToken();
