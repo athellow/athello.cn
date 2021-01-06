1d46473b28b876d86498814aa4fd0f7827fc8a9d
Athellow <athellow@outlook.com>
2021-01-06 17:05:06 +0800

    no message

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -25,7 +25,7 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-        $cocde = $request->input('code');
+        $code = $request->input('code');
 
         if ($code != '') {
             $appid = config('main.wechat.appid');
