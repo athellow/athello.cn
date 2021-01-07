458fbd03a38cdc9726a29f5fe4c17a19eda98b98
Athellow <athellow@outlook.com>
2021-01-07 14:41:21 +0800

    no message

--- app/Http/Controllers/Api/IlogController.php
+++ app/Http/Controllers/Api/IlogController.php
@@ -13,16 +13,19 @@ class IlogController extends Controller
 
     public function index(Request $request)
     {
-        $user_id = $request->input('user_id');
+        $user_id = $request->input('user_id', 0);
 
-        $user = User::find($user_id);
-        $list = $user->ilogs()->where('is_draft', 0)->get()->orderBy('published_at', 'desc')->simplePaginate(5);
+        // $user = User::find($user_id);
+        // $list = $user->ilogs()->where('is_draft', 0)->orderBy('published_at', 'desc')->simplePaginate(5);
 
-        // $list = Ilog::where(['user_id' => $user_id, 'is_draft' => 0])->orderBy('published_at', 'desc')->simplePaginate(5);
+        $list = Ilog::leftJoin('users','ilogs.user_id','users.id')
+            ->select('ilogs.*', 'users.wechat_name', 'users.wechat_avatar')
+            ->where(['users.id' => $user_id, 'ilogs.is_draft' => 0])->orderBy('ilogs.published_at', 'desc')
+            ->simplePaginate(5);
 
         $data = [
             'status' => 0,
-            'data' => $list->items()
+            'data' => $list ? $list->items() : []
         ];
 
         return response()->json($data);
